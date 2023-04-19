0d4777f258360524e36c1545606f6ede621626f8
José Henrique Colombo <jhbcolombo@hotmail.com>
2023-04-19 17:20:32 -0300

    Adicionado Entity Framework e Migrations

--- DemoMinimalAPI/DemoMinimalAPI/Program.cs
+++ DemoMinimalAPI/DemoMinimalAPI/Program.cs
@@ -1,9 +1,28 @@
+using DemoMinimalAPI.Data;
+using DemoMinimalAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MiniValidation;
+using NetDevPack.Identity;
+using NetDevPack.Identity.Jwt;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentityEntityFrameworkContextConfiguration(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("DemoMinimalAPI")));
+
+builder.Services.AddIdentityConfiguration();
+builder.Services.AddJwtConfiguration(builder.Configuration, "AppSettings");
 
 var app = builder.Build();
 
@@ -14,6 +33,90 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthConfiguration();
 app.UseHttpsRedirection();
+
+#region Mapeamento API
+app.MapGet("/fornecedor", async (
+MinimalContextDb context) =>
+await context.Fornecedores.ToListAsync())
+.WithName("GetFornecedor")
+.WithTags("Fornecedor");
+
+app.MapGet("/fornecedor/{id}", async (
+    Guid id,
+    MinimalContextDb context) =>
+    await context.Fornecedores.FindAsync(id)
+        is Fornecedor fornecedor ? Results.Ok(fornecedor) : Results.NotFound())
+    .Produces<Fornecedor>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("GetFornecedorPorId")
+    .WithTags("Fornecedor");
+
+app.MapPost("/fornecedor", async (
+        MinimalContextDb context,
+        Fornecedor fornecedor) =>
+{
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
+    context.Fornecedores.Add(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0 
+        ? Results.CreatedAtRoute("GetFornecedorPorId", new { id = fornecedor.Id}, fornecedor)
+        : Results.BadRequest("Problemas ao inserir registro");
+})
+    .ProducesValidationProblem()
+    .Produces<Fornecedor>(StatusCodes.Status201Created)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("PostFornecedor")
+    .WithTags("Fornecedor");
+
+app.MapPut("/fornecedor/{id}", async (
+    Guid id,
+    MinimalContextDb context,
+    Fornecedor fornecedor) =>
+{
+    var fornecedorBanco = await context.Fornecedores.AsNoTracking<Fornecedor>()
+                                                    .FirstOrDefaultAsync(f => f.Id == id);
+    if (fornecedorBanco == null) return Results.NotFound();
+
+    if(!MiniValidator.TryValidate(fornecedor, out var errors)) return Results.ValidationProblem(errors);
+
+    context.Fornecedores.Update(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Problemas ao atualizar registro");
+})
+    .ProducesValidationProblem()
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("PutFornecedor")
+    .WithTags("Fornecedor");
+
+app.MapDelete("/fornecedor/{id}", async (
+    Guid id,
+    MinimalContextDb context) =>
+{
+    var fornecedorBanco = await context.Fornecedores.FindAsync(id);
+    if (fornecedorBanco == null) return Results.NotFound();
+
+    context.Fornecedores.Remove(fornecedorBanco);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Problemas ao deletar registro");
+})
+    .ProducesValidationProblem()
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status400BadRequest)
+    //.RequireAuthorization("ExcluirFornecedor")
+    .WithName("DeleteFornecedor")
+    .WithTags("Fornecedor");
+#endregion
 
 app.Run();
